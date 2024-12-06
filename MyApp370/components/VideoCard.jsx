30f13dca97edd426eee8f3e67da68c5d86613474
olael94 <104049815+olael94@users.noreply.github.com>
2024-12-05 23:42:52 -0700

    3.11 FlatList and Custom Hooks
    - The first 4 steps of the assignment I made them in the previous commit.
    - Further Developed the VideoCard.jsx component to display individual video information (thumbnail, title, creator, play button)
    - Managed the play state with a play useState hook
    - Inserted Touchable opacity to press play on the videos.

--- MyApp370/components/VideoCard.jsx
+++ MyApp370/components/VideoCard.jsx
@@ -1,10 +1,65 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import {View, Text, Image, TouchableOpacity} from 'react-native'
+import React, {useState} from 'react'
 
-const VideoCard = ({ video: { title, thumbnail, video, creator: { username, avatar } } }) => {
+import {icons} from '../constants'
+
+const VideoCard = ({video: {title, thumbnail, video, creator: {username, avatar}}}) => {
+    //This state will be used to play the video
+    const [play, setPlay] = useState(false)
+
     return (
         <View className="floex-col items-center px-4 mb-14">
-            <Text className="text-2xl text-white ">{title}</Text>
+            <View className="flex-row gap-3 items-start">
+                <View className="justify-center items-center flex-row flex-1">
+                    <View
+                        className="w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5">
+                        <Image
+                            source={{uri: avatar}}
+                            className="w-full h-full rounded-lg"
+                            resizemode="cover"
+                        />
+                    </View>
+
+                    <View className="justify-center flex-1 ml-3 gap-y-1">
+                        <Text className="text-white font-psemibold text-sm" numberOfLines={1}>
+                            {title}
+                        </Text>
+                        <Text className="text-xs text-gray-100 font-pregular" numerOfLines={1}>
+                            {username}
+                        </Text>
+                    </View>
+                </View>
+
+                <View className="pt-2">
+                    <Image
+                        source={icons.menu}
+                        className="w-5 h-5"
+                        resizeMode="contain"
+                    />
+                </View>
+            </View>
+            {/*If play is true, show the video playing, else show the thumbnail*/}
+            {play ? (
+                <Text className="text-white">Playing</Text>
+            ) : (
+                <TouchableOpacity
+                    activeOpacity={0.7}
+                    //On press, set play to true
+                    onPress={() => setPlay(true)}
+                    className="w-full h-60 rounded-xl mt-3 relative justify-center items-center"
+                >
+                    <Image
+                        source={{uri: thumbnail}}
+                        className="w-full h-full rounded-xl mt-3"
+                        resizeMode="cover"
+                    />
+                    <Image
+                        source={icons.play}
+                        className="w-12 h-12 absolute"
+                        resizeMode="contain"
+                    />
+                </TouchableOpacity>
+            )}
         </View>
     )
 }
